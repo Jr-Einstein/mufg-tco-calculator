45886854e16d28ba2f9a9b076e0214c647443eb5
Aman Kumar Singh <amankumar.singh2022@vitbhopal.ac.in>
2026-03-14 17:52:39 +0530

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -2,15 +2,20 @@ import streamlit.web.cli as stcli
 import os, sys
 
 def resolve_path(path):
-    resolved_path = getattr(sys, '_MEIPASS', os.path.abspath(os.path.curdir))
-    return os.path.join(resolved_path, path)
+    """Get absolute path to resource, works for dev and for PyInstaller"""
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, path)
 
 if __name__ == "__main__":
-    # This points to your main app.py
+    # Pointing Streamlit to the app.py location inside the EXE
     sys.argv = [
         "streamlit",
         "run",
-        resolve_path("App.py"),
+        resolve_path("app.py"),
         "--global.developmentMode=false",
     ]
     sys.exit(stcli.main())
